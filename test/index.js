e292436762ee92fc0d60139443ec077e78b83101
Dustan Kasten <dustan.kasten@gmail.com>
2014-12-16 12:06:05 -0500

    initial commit

---
+++ test/index.js
@@ -0,0 +1,26 @@
+/*global it*/
+var assert = require('assert');
+var exec = require('child_process').exec;
+var tocjs = require('..');
+var fs = require('fs');
+var path = require('path');
+var read = fs.readFileSync;
+var readdir = fs.readdirSync;
+
+describe('should transform', function(){
+  readdir('test/cases').forEach(function(file){
+    if (file.indexOf('.out') > -1) return;
+    var base = path.basename(file, '.js');
+    var f = 'test/cases/' + file;
+    var input = read(f, 'utf8');
+    var output = read('test/cases/' + base + '.out.js', 'utf8');
+    it(base, function(done) {
+      tocjs(f, {dryrun: true}, function(err, file, transformed) {
+        assert.equal(transformed.trim(), output.trim());
+        done();
+      });
+    })
+  });
+})
+
+
